31f82dc8fdde4914e7666208d489430b2ed9e5df
MioBestWaifu <Fenix1603@hotmail.com>
2022-11-15 13:58:30 -0300

    Update Menu.java

--- Escola.java
+++ Escola.java
@@ -4,25 +4,26 @@ public class Escola{
     public HashMap<Integer,Docente> docentes;
     public HashMap<Integer,Disciplina> disciplinas;
     public HashMap<Integer,Aluno> alunos;
-    public HashMap<Integer,Turma> turmas;;
+    public HashMap<Integer,Turma> turmas;
     private GerenteDeArquivo ga;
 
     //Metódos
-    public void adicionarAluno(){}
+    //TODO fazer os métodos de adição e adição darem throws qnd acessar um item q nao existem
+    public void adicionarAluno(Aluno toAdd){}
 
-    public void removerAluno(){}
+    public void removerAluno(int codigo){}
 
-    public void adicionarDocente(){}
+    public void adicionarDocente(Docente toAdd){}
 
-    public void removerDocente(){}
+    public void removerDocente(int codigo){}
 
-    public void adicionarDisciplina(){}
+    public void adicionarDisciplina(Disciplina toAdd){}
 
-    public void removerDisciplina(){}
+    public void removerDisciplina(int codigo){}
 
-    public void adicionarTurma(){}
+    public void adicionarTurma(Turma toAdd){}
 
-    public void removerTurma(){}
+    public void removerTurma(int codigo){}
 
     //Os métodos abaixo estão retornando null enquanto não são devidamente implementados, pra não dar erro no java
 
@@ -34,14 +35,14 @@ public class Escola{
 
     public String mapearTurmas(){return null;}//Retorna o nome das turmas e seus códigos.
 
-    public void editarDocente(){}//Permite mudar informações de um docente, incluindo disciplinas.
+    public void editarDocente(int codigo){}//Permite mudar informações de um docente, incluindo disciplinas.
 
-    public void editarDisciplina(){}//Permite mudar informações de uma disciplina, incluindo docentes, alunos e notas. 
+    public void editarDisciplina(int codigo){}//Permite mudar informações de uma disciplina, incluindo docentes, alunos e notas. 
     //Não deixar disciplina ficar sem docente. Não deixar adicionar aluno de ano errado ser cadastrado.
 
-    public void editarAluno(){}//Permite mudar informações de um aluno, incluindo turmas.
+    public void editarAluno(int codigo){}//Permite mudar informações de um aluno, incluindo turmas.
 
-    public void editarTurma(){}//Permite mudar informações de um docente, incluindo alunos.
+    public void editarTurma(int codigo){}//Permite mudar informações de um docente, incluindo alunos.
 
     public void gerarRelatórios(int tipo){}//Gerar relatórios com o GeradorDeRelatorio e passar pro arquivo com o GerenteDeArquivos.
 
