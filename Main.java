9ab9fa50cdf53a09c8f4b2125220894dbd36f5c3
MioBestWaifu <Fenix1603@hotmail.com>
2022-12-05 15:20:55 -0300

    Mapeamento

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.security.Principal;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,18 +10,17 @@ public class Main {
          * }
          */
         
-        Menu menu = new Menu();
-		
-		System.out.print("1 - Menu do aluno\n2- ...\n3 - ...\n Digite: ");
+        //Menu menu = new Menu();
+		//System.out.print("1 - Menu do aluno\n2- ...\n3 - ...\n Digite: ");
 		//int op = Util.lerInteger();
 		//System.out.println(op);
         //System.out.println(Util.lerString());
-        System.out.println(Util.lerString());
-        System.out.println(Util.lerInteger());
-        System.out.println(Util.lerString());
-        System.out.println(Util.lerInteger());
-        System.out.println(Util.lerFloat());
-        System.out.println(Util.lerFloat());
+        // System.out.println(Util.lerString());
+        // System.out.println(Util.lerInteger());
+        // System.out.println(Util.lerString());
+        // System.out.println(Util.lerInteger());
+        // System.out.println(Util.lerFloat());
+        // System.out.println(Util.lerFloat());
 		/*switch(op) {
 		
 			case 1: menu.alunoMenu();
