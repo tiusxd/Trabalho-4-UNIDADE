0aa1b4caa1a5dce4577f1bac419b8d2d22eb903a
OzyHil <117992907+OzyHil@users.noreply.github.com>
2022-12-21 18:55:22 -0300

    Delete ClconeTeste directory

--- ClconeTeste/src/main/java/NoSuchKeyException.java
+++
@@ -1,6 +0,0 @@
-public class NoSuchKeyException extends Exception {
-    @Override
-    public String toString() {
-        return "Essa Key não existe caralho";
-    }
-}
--- ClconeTeste/src/main/java/Util.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Util{
-
-    public static final int ALUNO_LISTA = 0, DOCENTE_LISTA = 1, DISCPLINA_LISTA = 2, TURMA_LISTA = 3;
-    
-}
-
